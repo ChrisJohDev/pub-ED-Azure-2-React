cd20a64a0aff1b956ead83173d98ff6793550eb4
ChrisJohDev <chris@chrisjohannesson.com>
2021-07-05 14:00:37 +0200

    First commit from VS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Unit from './components/unit';
 import Footer from './components/footer';
 
 function App() {
-  return (
-    <div className="App">
-      <div class="page_wrapper">
-        <navigation>
-          <Nav />
-        </navigation>
-        <div class="page_container_wrapper container">
-          <Unit />
+    return (
+        <div className="App">
+            <div class="page_wrapper">
+                <navigation>
+                    <Nav />
+                </navigation>
+                <div class="page_container_wrapper container">
+                    <Unit />
+                </div>
+                <footer>
+                    <Footer />
+                </footer>
+            </div>
         </div>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
